20fec1d1845be36c227bb3648a4a24806b8fab96
BePsvPT <freedom@csie.io>
2018-05-23 20:38:35 +0800

    add download model

---
+++ app/Download.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Download extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
