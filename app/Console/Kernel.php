de892ea74383bdc19e7b81fc082fa85bfdb0a9af
BePsvPT <freedom@csie.io>
2018-05-30 19:29:47 +0800

    add sync package downloads command to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sync:packages-list')->dailyAt('02:00');
+        $schedule->command('sync:package-downloads')->dailyAt('03:00');
     }
 
     /**
