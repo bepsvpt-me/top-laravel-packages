4ecd458969a6a434293205fe1fdbf9bd8a38bb4e
BePsvPT <freedom@csie.io>
2018-06-18 16:33:08 +0800

    add weights column to packages table

---
+++ database/migrations/2018_06_18_070151_add_weights_column_to_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWeightsColumnToPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->tinyInteger('weights')
+                ->unsigned()
+                ->default(1)
+                ->after('favers')
+                ->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->dropColumn('weights');
+        });
+    }
+}
