bd9bf76da4983fdbc4ceb3bd15475d3805729042
BePsvPT <bepsvpt@protonmail.com>
2019-10-07 19:46:41 +0800

    fix table headers and add x scroll

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,16 @@
-if ($('#top-packages').length) {
-  let table = $('#top-packages').DataTable({
-    'pageLength': 100,
-    'order': [[1, 'desc'], [2, 'desc']],
-    'columns': [
-      { 'searchable': false, 'orderable': false },
-      { 'searchable': false },
-      { 'searchable': false },
-      null,
-      { 'orderable': false },
-      null,
-      null
-    ]
-  });
-
-  table.on('order.dt search.dt', function () {
-    table.column(0, { search:'applied', order:'applied' }).nodes().each(function (cell, i) {
-      cell.innerHTML = i + 1;
-    });
-  }).draw();
-}
+$('#top-packages').DataTable({
+  columns: [
+    { 'searchable': false, 'orderable': false },
+    { 'searchable': false },
+    { 'searchable': false },
+    null,
+    { 'orderable': false },
+    null,
+    null,
+  ],
+  fixedHeader: true,
+  order: [[1, 'desc'], [2, 'desc']],
+  pageLength: 100,
+  responsive: true,
+  scrollX: true,
+});
