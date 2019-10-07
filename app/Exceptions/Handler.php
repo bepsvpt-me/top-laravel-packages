bd0f92502aa7cbe0fe7a7cf6521dc4a6bad6c4a4
BePsvPT <bepsvpt@protonmail.com>
2019-10-07 20:00:59 +0800

    cleanup

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -29,8 +31,11 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param Exception $exception
+     *
      * @return void
+     *
+     * @throws Exception
      */
     public function report(Exception $exception)
     {
@@ -40,9 +45,10 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Exception $exception
+     *
+     * @return Response
      */
     public function render($request, Exception $exception)
     {
