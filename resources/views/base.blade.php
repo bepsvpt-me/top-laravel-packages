a4240e84149a2072fc87982915ab99ece6ff63d2
BePsvPT <bepsvpt@protonmail.com>
2019-10-07 19:48:07 +0800

    update assets links

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/datatables.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Top Laravel Packages</title>
+    <title>Top 1000 Laravel Packages</title>
   </head>
   <body>
     <header class="container-fluid">
       <div class="row align-items-end mt-4">
         <div class="col">
-          <h1 class="m-0">
-            <span class="homepage">@yield('title')</span>
-          </h1>
+          <h1 class="m-0">@yield('title')</h1>
         </div>
 
-        <div class="col-4">
+        <div class="col-12 col-md-4">
           @yield('header')
         </div>
       </div>
@@ -33,22 +31,17 @@
       <div class="container-fluid text-center text-muted">
         <span>Copyright © {{ date('Y') }}</span>
 
-        <span> · </span>
+        <span class="mx-1">·</span>
 
-        <span>
-          <a href="https://github.com/BePsvPT/top-laravel-packages" target="_blank" rel="noopener">
-            GitHub
-          </a>
-        </span>
+        <a
+          href="https://github.com/BePsvPT/top-laravel-packages"
+          target="_blank"
+          rel="noopener"
+        >GitHub</a>
       </div>
     </footer>
 
-    <script src="{{ asset('js/jquery-3.3.1.slim.min.js') }}" defer></script>
-    <script src="{{ asset('js/popper.min.js') }}" defer></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
-    <script src="{{ asset('js/jquery.dataTables.min.js') }}" defer></script>
-    <script src="{{ asset('js/dataTables.bootstrap4.min.js') }}" defer></script>
-    <script src="{{ asset('js/URI.min.js') }}" defer></script>
+    <script src="{{ asset('js/datatables.min.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
   </body>
 </html>
