59c66e5bce72cc2088b14782b7e19d1176942059
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-08 00:53:36 +0800

    add dot and external link component views

---
+++ resources/views/components/dot.blade.php
@@ -0,0 +1,1 @@
+<span class="text-black-50 mx-1" style="user-select: none;">•</span>
---
+++ resources/views/components/external-link.blade.php
@@ -0,0 +1,9 @@
+<a
+  class="text-decoration-none"
+  href="{{ $href }}"
+  referrerpolicy="no-referrer"
+  rel="noopener noreferrer"
+  target="_blank"
+>
+  {{ $slot }}
+</a>
