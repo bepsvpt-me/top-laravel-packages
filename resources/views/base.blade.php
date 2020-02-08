d80fe2ebdc4d03cc18ddf647fe6be586e6e0749d
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-08 09:21:07 +0800

    prepare for front-end building

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title') | Laravel Packages</title>
   </head>
   <body class="d-flex flex-column">
