5177f7f9e7f5d4eb9ab4debf109670d353b34000
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-12 20:56:43 +0800

    add version to third party files link

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css?v=4.4.1') }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title') | Laravel Packages</title>
   </head>
