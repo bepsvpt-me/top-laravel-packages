868c16d0ac121e49bbed1fc40f2c1130cd7db1e6
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-21 19:12:20 +0800

    seo

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="List top 1,000 downloads Laravel packages.">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title') | Laravel Packages</title>
   </head>
