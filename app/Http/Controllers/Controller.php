08550c1f4dffd871cc994dd73036247fb17590bc
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-03-07 14:59:57 +0800

    let base controller abstract

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use DispatchesJobs;
 }
