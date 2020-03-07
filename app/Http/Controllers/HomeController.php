77c2900a89d9339af6d165ec6ea9c7d70d7ef78c
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-03-07 14:49:42 +0800

    update official include packages list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -104,6 +104,7 @@ class HomeController extends Controller
     {
         $officialIncludes = [
             'facade/ignition',
+            'fruitcake/laravel-cors',
             'nunomaduro/collision',
         ];
 
