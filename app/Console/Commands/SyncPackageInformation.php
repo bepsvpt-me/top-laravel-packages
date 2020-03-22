fd0bd0715d9a02dcb04a9fed5579d9004494ff14
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-03-22 18:11:47 +0800

    add laravel 8.0

--- app/Console/Commands/SyncPackageInformation.php
+++ app/Console/Commands/SyncPackageInformation.php
@@ -178,7 +178,7 @@ final class SyncPackageInformation extends Command
     protected function minLaravelVersion(string $constraint = null): ?string
     {
         $versions = [
-            '7.0', '6.0',
+            '8.0', '7.0', '6.0',
             '5.8', '5.7', '5.6', '5.5', '5.4', '5.3', '5.2', '5.1', '5.0',
             '4.2', '4.1', '4.0',
         ];
