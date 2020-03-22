7c3cde5ad4ce172418f7b7123befab39f01eeed6
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-03-22 20:04:14 +0800

    update package factory

--- database/factories/PackageFactory.php
+++ database/factories/PackageFactory.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Package;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
 
 /** @var Factory $factory */
 
-$factory->define(App\Package::class, function (Faker $faker) {
+$factory->define(Package::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'description' => $faker->sentence,
