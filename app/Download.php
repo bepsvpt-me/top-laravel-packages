0d0013d7c3bd73959898a4f2a57880801f3c400d
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-12-03 21:42:15 +0800

    update phpdoc

--- app/Download.php
+++ app/Download.php
@@ -2,13 +2,30 @@
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
+ * App\Download
+ *
+ * @property int $id
+ * @property int $package_id
  * @property string $date
- * @property Package $package
+ * @property int $downloads
+ * @property string $type
+ * @property-read Package $package
+ * @method static Builder|Download newModelQuery()
+ * @method static Builder|Download newQuery()
+ * @method static Builder|Download query()
+ * @method static Builder|Download whereDate($value)
+ * @method static Builder|Download whereDownloads($value)
+ * @method static Builder|Download whereId($value)
+ * @method static Builder|Download wherePackageId($value)
+ * @method static Builder|Download whereType($value)
+ * @mixin Eloquent
  */
 final class Download extends Model
 {
